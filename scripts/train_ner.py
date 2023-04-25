bceb0af3e1ee37c1d1a406b07da50223d2ab33e2
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 13:39:39 +0900

    Update train_ner.py

--- scripts/train_ner.py
+++ scripts/train_ner.py
@@ -26,7 +26,7 @@ for file_name in os.listdir(ner_data_dir):
         with open(os.path.join(ner_data_dir, file_name), "r") as f:
             lines = f.readlines()
             tokens = [line.split()[0] for line in lines]
-            labels = [line.split()[1] for line in lines]
+            labels = [line.split()[1] for line in lines if len(line.split()) > 1]
             all_labels.extend(labels)
             encoded = tokenizer.encode_plus(tokens, add_special_tokens=True, padding="max_length", truncation=True, max_length=128, return_tensors="pt")
             ner_input_ids.append(encoded["input_ids"])
