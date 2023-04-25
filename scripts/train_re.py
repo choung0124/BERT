d49f2bcf4a8232c6a107b9d9b2de3464f49fa292
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 17:04:26 +0900

    Update train_re.py

--- scripts/train_re.py
+++ scripts/train_re.py
@@ -42,6 +42,9 @@ relations = [
 ]
 relation_to_id = {relation: idx for idx, relation in enumerate(relations)}
 
+with open("relation_to_id.json", "w") as f:
+    json.dump(relation_to_id, f, indent=4)
+
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 re_model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=len(relation_to_id))
 
@@ -106,7 +109,4 @@ os.makedirs(output_dir, exist_ok=True)
 re_model.save_pretrained(output_dir)
 tokenizer.save_pretrained(output_dir)
 
-mapping_file = "models/ner/relation_to_id.json"
 
-with open(mapping_file, "w") as f:
-    json.dump(relation_to_id, f)
