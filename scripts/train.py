3d3de2cb4dfc17fc4e9bf1adaf1b7853f6cd9612
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 11:37:47 +0900

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -4,15 +4,16 @@ from torch.optim import AdamW
 from tqdm import tqdm
 from transformers import BertTokenizerFast
 from model_definition import NER_RE_Model
-from data_prep import tokenize_our_data, create_data_loader, NERRE_Dataset, process_directory
+from data_prep import tokenize_data, create_data_loader, NERRE_Dataset, process_directory
 import os
+import pickle
 
 from generate_label_dicts import generate_label_dicts
 
 def train_epoch(model, data_loader, optimizer, device):
     model.train()
     for batch in tqdm(data_loader):
-        input_ids, attention_mask, ner_label, re_label, subject_positions, object_positions = batch
+        input_ids, attention_mask, ner_label, re_label = batch
         input_ids = input_ids.to(device)
         attention_mask = attention_mask.to(device)
         ner_labels = ner_label.to(device)
@@ -27,7 +28,6 @@ def train_epoch(model, data_loader, optimizer, device):
         total_loss.backward()
         optimizer.step()
 
-
 dir_path = "test"
 subject_label2idx, object_label2idx, re_label2idx, ner_label2idx = generate_label_dicts(dir_path)
 
@@ -35,7 +35,7 @@ filtered_data = process_directory(dir_path, subject_label2idx, object_label2idx,
 train_dataset = NERRE_Dataset(*zip(*filtered_data))
 
 tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
-tokenized_train_data = tokenize_our_data(train_dataset)
+tokenized_train_data = tokenize_data(train_dataset)
 train_data_loader = DataLoader(tokenized_train_data, batch_size=8, shuffle=True)
 
 model = NER_RE_Model(len(ner_label2idx), len(re_label2idx), ner_label2idx, re_label2idx)
@@ -55,7 +55,6 @@ for epoch in range(num_epochs):
     train_epoch(model, train_data_loader, optimizer, device)
 
 model_dir = "model"
-# Save the pretrained BERT model
 model.bert.save_pretrained(model_dir)
 
 # Save the custom head
@@ -79,4 +78,3 @@ label_dicts = {
 
 with open(os.path.join(model_dir, 'label_dicts.pkl'), 'wb') as f:
     pickle.dump(label_dicts, f)
-
