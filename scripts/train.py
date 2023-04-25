f26d80d89ea6c534204cbdfd085eff5281798949
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 10:45:34 +0900

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -4,20 +4,12 @@ from torch.optim import AdamW
 from tqdm import tqdm
 from transformers import BertTokenizerFast
 from model_definition import NER_RE_Model
-from data_prep import tokenize_data, create_data_loader, NERRE_Dataset, ner_label2idx, re_label2idx, filtered_data
+from data_prep import tokenize_data, NERRE_Dataset, ner_label2idx, re_label2idx, tokenized_train_data
 import os
 
-# Create the train_dataset with filtered data
-train_dataset = NERRE_Dataset(*zip(*filtered_data))
-
-tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
-tokenized_train_data = tokenize_data(train_dataset)
 train_data_loader = DataLoader(tokenized_train_data, batch_size=8, shuffle=True)
 
 model = NER_RE_Model(len(ner_label2idx), len(re_label2idx))
-
-batch_size = 8
-train_data_loader = create_data_loader(tokenized_train_data, batch_size)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
@@ -27,20 +19,24 @@ optimizer = AdamW(model.parameters(), lr=3e-5)
 def train_epoch(model, data_loader, optimizer, device):
     model.train()
     for batch in tqdm(data_loader):
-        input_ids, attention_mask, ner_label, re_label = batch
-        input_ids = input_ids.to(device)
-        attention_mask = attention_mask.to(device)
-        ner_labels = ner_label.to(device)
-        re_labels = re_label.to(device)
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        subject_labels = batch['subject_label'].to(device)
+        object_labels = batch['object_label'].to(device)
+        re_labels = batch['re_label'].to(device)
+        entity_positions = batch['entity_positions'].to(device)
 
         optimizer.zero_grad()
-        ner_logits, re_logits = model(input_ids, attention_mask)
+        subject_logits, object_logits, re_logits = model(input_ids, attention_mask, entity_positions)
         loss_fn = torch.nn.CrossEntropyLoss()
-        ner_loss = loss_fn(ner_logits, ner_labels)
+        subject_loss = loss_fn(subject_logits.view(-1, subject_logits.size(-1)), subject_labels.view(-1))
+        object_loss = loss_fn(object_logits.view(-1, object_logits.size(-1)), object_labels.view(-1))
         re_loss = loss_fn(re_logits, re_labels)
-        total_loss = ner_loss + re_loss
+        total_loss = subject_loss + object_loss + re_loss
         total_loss.backward()
         optimizer.step()
+
+# ... (rest of the code remains the same)
 
 num_epochs = 3
 
@@ -53,9 +49,13 @@ model.bert.save_pretrained(model_dir)
 
 # Save the custom head
 torch.save({
-    'ner_classifier': model.ner_classifier.state_dict(),
+    'subject_classifier': model.subject_classifier.state_dict(),
+    'object_classifier': model.object_classifier.state_dict(),
     're_classifier': model.re_classifier.state_dict()
 }, os.path.join(model_dir, 'custom_head.pt'))
 
 # Save the tokenizer
 tokenizer.save_pretrained(model_dir)
+
+# ... (rest of the code remains the same)
+
