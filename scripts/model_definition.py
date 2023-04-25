7a1d81874f91d0700afe692f4209d96f97a391f8
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 11:10:11 +0900

    Update model_definition.py

--- scripts/model_definition.py
+++ scripts/model_definition.py
@@ -3,22 +3,17 @@ from torch import nn
 from transformers import BertModel
 
 class NER_RE_Model(nn.Module):
-    def __init__(self, subject_label_count, object_label_count, re_label_count):
+    def __init__(self, ner_label_count, re_label_count, ner_label2idx, re_label2idx):
         super(NER_RE_Model, self).__init__()
         self.bert = BertModel.from_pretrained('bert-base-uncased')
-        self.subject_classifier = nn.Linear(self.bert.config.hidden_size, subject_label_count)
-        self.object_classifier = nn.Linear(self.bert.config.hidden_size, object_label_count)
-        self.re_classifier = nn.Linear(self.bert.config.hidden_size * 2, re_label_count)
+        self.ner_classifier = nn.Linear(self.bert.config.hidden_size, ner_label_count)
+        self.re_classifier = nn.Linear(self.bert.config.hidden_size, re_label_count)
+        self.ner_label2idx = ner_label2idx
+        self.re_label2idx = re_label2idx
 
-    def forward(self, input_ids, attention_mask, entity_positions):
+    def forward(self, input_ids, attention_mask):
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        sequence_output = outputs.last_hidden_state
-
-        subject_logits = self.subject_classifier(sequence_output)
-        object_logits = self.object_classifier(sequence_output)
-
-        entity_outputs = sequence_output[torch.arange(input_ids.shape[0]).unsqueeze(1), entity_positions]
-        entity_pair_output = torch.cat((entity_outputs[:, 0], entity_outputs[:, 1]), dim=1)
-        re_logits = self.re_classifier(entity_pair_output)
-
-        return subject_logits, object_logits, re_logits
+        pooled_output = outputs.pooler_output
+        ner_logits = self.ner_classifier(pooled_output)
+        re_logits = self.re_classifier(pooled_output)
+        return ner_logits, re_logits
