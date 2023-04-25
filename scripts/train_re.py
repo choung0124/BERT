1ec3467fedaf2f5bd7189a76846d1b3e8d8ab73b
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 14:53:17 +0900

    Update train_re.py

--- scripts/train_re.py
+++ scripts/train_re.py
@@ -15,58 +15,40 @@ num_epochs = 10
 batch_size = 16
 learning_rate = 2e-5
 
+# Create a mapping from relation labels to their respective IDs
+relation_to_id = {}
+
 # Tokenize the RE data and create a training set
 re_input_ids = []
 re_attention_masks = []
 re_labels = []
 
-# Create a mapping from relation labels to their respective IDs
-relation_to_id = {}
-
-# Open and read the training data file
-train_data_file = os.path.join(re_data_dir, "train_data.txt")
-with open(train_data_file, "r") as f:
-    lines = f.readlines()
-
-# Loop through each line of the training data
-for line in lines:
-    # Split the line into subject, object, relation
-    subject, obj, relation = line.strip().split("\t")
-    
-    # Add the relation to the dictionary if it doesn't exist yet
-    if relation not in relation_to_id:
-        relation_to_id[relation] = len(relation_to_id)
-
-# Tokenize and align the relations
-def extract_subject_relation_object(line):
-    words = line.split()
-    relation = words[-1]
-    obj = words[-2]
-    subject = ' '.join(words[:-2])
-    return subject, relation, obj
-
+# Loop through all files in the training_data directory
 for file_name in os.listdir(re_data_dir):
-    if file_name.endswith("_re_data.txt"):
-        with open(os.path.join(re_data_dir, file_name), "r") as f:
-            line = f.readline()
-            subject, relation, obj = extract_subject_relation_object(line)
+    if not file_name.endswith("_re_data.txt"):
+        continue
 
-            tokens = tokenizer.tokenize(f"{subject} [SEP] {obj}")
-            encoded = tokenizer.encode_plus(tokens, add_special_tokens=True, padding="max_length", truncation=True, max_length=128, return_tensors="pt")
-            re_input_ids.append(encoded["input_ids"])
-            if relation not in relation_to_id:
-                print(f"Error: Relation '{relation}' not found in the relation_to_id dictionary")
-                continue
+    # Read the relation data from the file
+    with open(os.path.join(re_data_dir, file_name), "r") as f:
+        line = f.readline()
+        subject, relation, obj = extract_subject_relation_object(line)
 
-            re_labels.append(torch.tensor(relation_to_id[relation]))
-
-
+        # Tokenize and encode the relation
+        tokens = tokenizer.tokenize(f"{subject} [SEP] {obj}")
+        encoded = tokenizer.encode_plus(tokens, add_special_tokens=True, padding="max_length", truncation=True, max_length=128, return_tensors="pt")
+        
+        # Add the encoded relation and its label to the lists
+        re_input_ids.append(encoded["input_ids"])
+        if relation not in relation_to_id:
+            relation_to_id[relation] = len(relation_to_id)
+        re_labels.append(torch.tensor(relation_to_id[relation]))
+        
+# Concatenate the input IDs and attention masks into tensors
 re_input_ids = torch.cat(re_input_ids, dim=0)
-re_attention_masks = torch.cat(re_attention_masks, dim=0)
-re_labels = torch.cat(re_labels, dim=0)
+re_attention_masks = torch.ones_like(re_input_ids)
 
 # Create a DataLoader for the RE data
-re_dataset = TensorDataset(re_input_ids, re_attention_masks, re_labels)
+re_dataset = TensorDataset(re_input_ids, re_attention_masks, torch.stack(re_labels))
 re_loader = DataLoader(re_dataset, batch_size=batch_size)
 
 # Fine-tune the BERT RE model
