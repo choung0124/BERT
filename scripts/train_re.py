6212211fbb7df5da336fe7ba311cf23d37cd4118
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 15:53:38 +0900

    Update train_re.py

--- scripts/train_re.py
+++ scripts/train_re.py
@@ -40,31 +40,29 @@ relation_to_id = {relation: idx for idx, relation in enumerate(relations)}
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 re_model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=len(relation_to_id))
 
+# Load and preprocess the RE data
+with open("re_data.json", "r") as f:
+    json_data = json.load(f)
+re_data = preprocess_re(json_data)
+
 # Tokenize the RE data and create a training set
 re_input_ids = []
 re_attention_masks = []
 re_labels = []
 
-# Loop through all files in the training_data directory
-for file_name in os.listdir(re_data_dir):
-    if not file_name.endswith("_re_data.txt"):
-        continue
+for data in re_data:
+    subject, relation, obj = data
 
-    # Read the relation data from the file
-    with open(os.path.join(re_data_dir, file_name), "r") as f:
-        for line in f:
-            subject, relation, obj = extract_subject_relation_object(line)
+    # Tokenize and encode the relation
+    tokens = tokenizer.tokenize(f"{subject} [SEP] {obj}")
+    encoded = tokenizer.encode_plus(tokens, add_special_tokens=True, padding="max_length", truncation=True, max_length=128, return_tensors="pt")
 
-            # Tokenize and encode the relation
-            tokens = tokenizer.tokenize(f"{subject} [SEP] {obj}")
-            encoded = tokenizer.encode_plus(tokens, add_special_tokens=True, padding="max_length", truncation=True, max_length=128, return_tensors="pt")
-            
-            if encoded["input_ids"].shape[1] > 0:
-                # Add the encoded relation and its label to the lists
-                re_input_ids.append(encoded["input_ids"])
-                re_attention_masks.append(encoded["attention_mask"])
-                re_labels.append(torch.tensor(relation_to_id[relation]))
-        
+    if encoded["input_ids"].shape[1] > 0:
+        # Add the encoded relation and its label to the lists
+        re_input_ids.append(encoded["input_ids"])
+        re_attention_masks.append(encoded["attention_mask"])
+        re_labels.append(torch.tensor(relation_to_id[relation]))
+
 # Concatenate the input IDs, attention masks, and labels into tensors
 re_input_ids = torch.cat(re_input_ids, dim=0)
 re_attention_masks = torch.cat(re_attention_masks, dim=0)
