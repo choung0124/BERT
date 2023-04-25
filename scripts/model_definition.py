a53bf61abdc6648af81d3c07a28b5b8fa17a220c
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 10:39:00 +0900

    Update model_definition.py

--- scripts/model_definition.py
+++ scripts/model_definition.py
@@ -7,11 +7,16 @@ class NER_RE_Model(nn.Module):
         super(NER_RE_Model, self).__init__()
         self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.ner_classifier = nn.Linear(self.bert.config.hidden_size, ner_label_count)
-        self.re_classifier = nn.Linear(self.bert.config.hidden_size, re_label_count)
+        self.re_classifier = nn.Linear(self.bert.config.hidden_size * 2, re_label_count)
 
-    def forward(self, input_ids, attention_mask):
+    def forward(self, input_ids, attention_mask, entity_positions):
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        pooled_output = outputs.pooler_output
-        ner_logits = self.ner_classifier(pooled_output)
-        re_logits = self.re_classifier(pooled_output)
+        sequence_output = outputs.last_hidden_state
+
+        ner_logits = self.ner_classifier(sequence_output)
+
+        entity_outputs = sequence_output[torch.arange(input_ids.shape[0]).unsqueeze(1), entity_positions]
+        entity_pair_output = torch.cat((entity_outputs[:, 0], entity_outputs[:, 1]), dim=1)
+        re_logits = self.re_classifier(entity_pair_output)
+
         return ner_logits, re_logits
